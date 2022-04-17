a7aaa643e2038aef8493abfcb3a0502e5e0924cf
jpalmat <jimmy.palma1990@gmail.com>
2022-04-17 16:44:17 -0500

    conditional in html code in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,27 +6,18 @@ class Counter extends Component {
         test: 'zero',
         tags: ['tag1', 'tag2', 'tag3']
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>No tags</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
     render() {
 
         return (
         <React.Fragment>
-            <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
+            {this.state.tags.length === 0 && "Create tags"}
+            { this.renderTags() }
         </React.Fragment>);
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes+= this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCounter() {
-    const { test, count } = this.state
-        return count === 0 ? test : count;
     }
 }
 export default Counter;
