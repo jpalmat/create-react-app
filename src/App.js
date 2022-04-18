656745903fc8fdc95c98b79909a0b55aedc061a1
jpalmat <jimmy.palma1990@gmail.com>
2022-04-18 16:03:39 -0500

    setting state in App component parent to share data with all the children

--- src/App.js
+++ src/App.js
@@ -4,12 +4,49 @@ import React, {Component} from 'react';
 import Counters  from './components/counters';
 
 class App extends Component {
+    state = {
+            counters: [
+                {id: 1, value: 0},
+                {id: 2, value: 4},
+                {id: 3, value: 0},
+                {id: 4, value: 0}
+            ]
+        }
+
+        handleIncrement = counter => {
+            const counters = [...this.state.counters];
+            const index = counters.indexOf(counter);
+            counters[index] = {...counter};
+            counters[index].value++;
+            this.setState({counters});
+
+        }
+
+        handleDelete = (counterId) => {
+            console.log('event handler called', counterId);
+            const counters = this.state.counters.filter(c => c.id !== counterId)
+            this.setState({counters})
+        }
+
+        handleReset = () => {
+            const counters = this.state.counters.map(c => {
+                c.value = 0;
+                return c;
+            });
+            this.setState({counters})
+        }
+
     render() {
     return (
         <React.Fragment>
-            <Navbar />
+            <Navbar totalCounters={this.state.counters.filter(c=> c.value>0).length}/>
             <main className="container">
-                <Counters />
+                <Counters
+                    counters={this.state.counters}
+                    onReset={this.handleReset}
+                    onDelete={this.handleDelete}
+                    onIncrement={this.handleIncrement}
+                />
             </main>
         </React.Fragment>
       );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,9 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-light bg-light">
              <div className="container-fluid">
-               <a className="navbar-brand" href="#">Navbar</a>
+               <a className="navbar-brand" href="#">
+                Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+               </a>
              </div>
             </nav>
         );
